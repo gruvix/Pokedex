bd9719fd4d61f2638b57b3b538ef6c89a2fed99a
Gruvix <timmy.burton@hotmail.com>
2023-10-25 19:14:16 -0300

    add test pokemon not found

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
+const { get } = require("jquery")
+
+describe('tests the pokedex', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:8000/')
   })
-})
+  it('shows an error if the pokemon is not found', () => {
+    cy.get('#pokemonIdInput').type('not a pokemon').get('#searchPokemonByIdButton').click()
+    cy.get('#pokemonNotFound').should('be.visible')
+
+  })
