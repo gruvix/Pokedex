68e3d16d137675b624ac0ac414b7f244390c6307
Gruvix <timmy.burton@hotmail.com>
2023-10-26 17:30:58 -0300

    add test to scroll through pokemon's sprites

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,3 @@
-const { get } = require("jquery")
-
 describe('tests the pokedex', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8000/')
@@ -9,7 +7,13 @@ describe('tests the pokedex', () => {
     cy.get('#pokemonNotFound').should('be.visible')
 
   })
-  it('gets a pokemon', () => {
+  it("gets a pokemon and scrolls through the pokedex's images", () => {
+    const CAROUSEL_DELAY = 700
     cy.get('#pokemonIdInput').type('pikachu').get('#searchPokemonByIdButton').click()
+    cy.get('#pokemonNotFound').should('not.be.visible')
+    cy.get('.carousel-inner').children().each((sprite, index) => {
+      cy.wait(CAROUSEL_DELAY).get('.carousel-control-next').click().get(sprite).should('be.visible')
+    })
+
   })
 })
