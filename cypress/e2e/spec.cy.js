63528b4dc1c9146138c5bf13f775fd892f2bc0ec
Gruvix <timmy.burton@hotmail.com>
2023-10-28 16:05:22 -0300

    refactor all html id's to kebab case

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,14 +3,14 @@ describe('tests the pokedex', () => {
     cy.visit('http://localhost:8000/')
   })
   it('shows an error if the pokemon is not found', () => {
-    cy.get('#pokemonIdInput').type('not a pokemon').get('#searchPokemonByIdButton').click()
-    cy.get('#pokemonNotFound').should('be.visible')
+    cy.get('#pokemon-id-input').type('not a pokemon').get('#search-pokemon-button').click()
+    cy.get('#pokemon-not-found').should('be.visible')
 
   })
   it("gets a pokemon and scrolls through the pokedex's images", () => {
     const CAROUSEL_DELAY = 700
-    cy.get('#pokemonIdInput').type('pikachu').get('#searchPokemonByIdButton').click()
-    cy.get('#pokemonNotFound').should('not.be.visible')
+    cy.get('#pokemon-id-input').type('pikachu').get('#search-pokemon-button').click()
+    cy.get('#pokemon-not-found').should('not.be.visible')
     cy.get('.carousel-inner').children().each((sprite) => {
       cy.wait(CAROUSEL_DELAY).get('.carousel-control-next').click().get(sprite).should('be.visible')
     })
