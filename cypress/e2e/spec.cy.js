3b5c8cafd22bef72b965472b7bc800ed9fefa2c6
Gruvix <timmy.burton@hotmail.com>
2023-10-31 10:20:36 -0300

    add to test select ranom pokemon from list
    the test requests a random list page
    then clicks a random pokemon from current page
    last it checks there are images loaded into the carousel
    it uses a variable from the main script,
    using cypress.env
    there is now a global variable to keep track of the total amount of pokemons

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,7 +7,27 @@ describe('tests the pokedex', () => {
     cy.get('#pokemon-not-found').should('be.visible')
 
   })
-  it("gets a pokemon and scrolls through the pokedex's images", () => {
+
+  it('clicks a random pokemon from the list', () => {
+    const POKEMON_LIST_AMOUNT = 15;
+    const TOTAL_POKEMON = Cypress.env('totalPokemon')
+    const LIST_TOTAL_OFFSET = TOTAL_POKEMON - POKEMON_LIST_AMOUNT;
+    const LIST_RANDOM_OFFSET = Math.floor(Math.random() * LIST_TOTAL_OFFSET);
+    const FIRST_POKEMON_IN_LIST = LIST_RANDOM_OFFSET + 1;
+    cy.window().then((win) => {
+      win.getPokemonList(LIST_RANDOM_OFFSET, POKEMON_LIST_AMOUNT)
+      cy.get("#pokemon-list li").first().invoke('text').should(text => {
+        const index = parseInt(text.split('.'));
+        expect(index).to.equal(FIRST_POKEMON_IN_LIST)
+      });
+    }).get("#pokemon-list li").should('have.length', POKEMON_LIST_AMOUNT).then((list) => {
+      const randomPokemonIndex = Math.floor(Math.random() * list.length);
+      const randomPokemon = list[randomPokemonIndex];
+      cy.wrap(randomPokemon).children().first().click();
+    }).get("#pokemon-carousel .carousel-inner").should('have.length.gt', 0);
+  })
+
+  it("gets pikachu and scrolls through the pokedex's images", () => {
     const CAROUSEL_DELAY = 700
     cy.get('#pokemon-id-input').type('pikachu').get('#search-pokemon-button').click()
     cy.get('#pokemon-not-found').should('not.be.visible')
