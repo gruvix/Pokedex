ea9adbe090cf8928ec9fe4cca28d10e529d1cb98
Gruvix <timmy.burton@hotmail.com>
2023-11-02 16:49:53 -0300

    update pokemon abilities to remove old abilities when updating pokemon

--- src/main.js
+++ src/main.js
@@ -139,8 +139,9 @@ function pokemonHandler(pokemon){
     displayPokemon(pokemon.sprites.other);
 }
 function updateAbilities(abilities){
+    $('#pokemon-abilities').children().empty();
     for(const ability of abilities){
-        const div  = $(`<div>${ability.ability.name}</div>`);
+        const div  = $(`<div><i>-${ability.ability.name}</i></div>`);
         $('#pokemon-abilities').append(div);
     }
 }
