d0cc10e8ee6229a3a0e08b523115f62971ffdd81
Gruvix <timmy.burton@hotmail.com>
2023-11-14 19:47:19 -0300

    Create display.js
    moved display main pokemon related functions to new file

--- src/main.js
+++ src/main.js
@@ -114,29 +114,6 @@ function clearPokemonList() {
   list.children().remove();
 }
 
-function displayPokemon(sprites) {
-  Object.keys(sprites).forEach((spriteCategory) => {
-    Object.keys(sprites[spriteCategory]).forEach((sprite) => {
-      if (sprites[spriteCategory][sprite]) {
-        addImageToCarousel(sprites[spriteCategory][sprite]);
-      }
-    });
-  });
-}
-
-function addImageToCarousel(imageSource) {
-  const img = document.createElement('img');
-  img.src = imageSource;
-  const div = document.createElement('div');
-  div.appendChild(img);
-  const carousel = document.querySelector('#pokemon-carousel .carousel-inner');
-  carousel.appendChild(div);
-  div.classList.add('carousel-item');
-  if (carousel.children.length === 1) {
-    div.classList.add('active');
-  }
-}
-
 function clearPokemon() {
   $('#pokemon-carousel .carousel-inner').children().remove();
   $('#pokemon-name').removeClass();
