614a6cfc110ef564dd72fb5a19d13219812320e8
Gruvix <timmy.burton@hotmail.com>
2023-11-15 15:17:56 -0300

    move generateRandomId to utils.js
    created file for utility functions named utils

--- src/main.js
+++ src/main.js
@@ -33,13 +33,6 @@ updatePokemons();
  * @param {number} [amount=15] - The maximum number of Pokemon to fetch.
  */
 
-function generateRandomId() {
-  const POKEMON_PAGE_AMOUNT = 15;
-  const POKEMON_TOTAL = $('#pokemon-now-showing').attr('data-total');
-  const RANDOM_ID = Math.floor(Math.random() * POKEMON_TOTAL);
-  return RANDOM_ID - POKEMON_PAGE_AMOUNT;
-}
-
 // ## construir un pokedex
 // Documentacion https://pokeapi.co/
 // listar pokemons y poder cambiar de página
