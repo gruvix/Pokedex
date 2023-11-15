94d4db6a21384ddbc9c99eb14a5914616c30add9
Timothy <72170232+gruvix@users.noreply.github.com>
2023-11-15 16:04:30 -0300

    Revert "add test pokemon not found"
    This reverts commit bd9719fd4d61f2638b57b3b538ef6c89a2fed99a.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,5 @@
-const { get } = require("jquery")
-
-describe('tests the pokedex', () => {
-  beforeEach(() => {
+describe('template spec', () => {
+  it('passes', () => {
     cy.visit('http://localhost:8000/')
   })
-  it('shows an error if the pokemon is not found', () => {
-    cy.get('#pokemonIdInput').type('not a pokemon').get('#searchPokemonByIdButton').click()
-    cy.get('#pokemonNotFound').should('be.visible')
-
-  })
+})
