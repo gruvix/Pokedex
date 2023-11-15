a76fff62b70a52e4ac3a2dcdfadda7542b7b7292
Gruvix <timmy.burton@hotmail.com>
2023-11-15 16:17:14 -0300

    Update main.js
    renamed namespace pokeList to pokemonList to be more descriptive

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 /* eslint-disable import/extensions */
 /// <reference types="jquery" />
-import * as pokeList from './pokemonList.js';
+import * as pokemonList from './pokemonList.js';
 import getPokemonHandler from './pokemon.js';
 import generateRandomId from './utils.js';
 
 $('#random-pokemon-button').on('click', () => {
   const random = generateRandomId();
-  pokeList.updatePokemons(random);
+  pokemonList.updatePokemons(random);
 });
 $('#search-pokemon-button').on('click', () => {
   const pokemon = $('#pokemon-id-input').val().toLowerCase();
@@ -14,19 +14,19 @@ $('#search-pokemon-button').on('click', () => {
 });
 $('#previous-page').on('click', () => {
   const PREVIOUS_PAGE_OFFSET = 16;
-  const previousPage = pokeList.getFirstPokemonOnListId() - PREVIOUS_PAGE_OFFSET;
-  pokeList.updatePokemons(previousPage);
+  const previousPage = pokemonList.getFirstPokemonOnListId() - PREVIOUS_PAGE_OFFSET;
+  pokemonList.updatePokemons(previousPage);
 });
 $('#next-page').on('click', () => {
-  const nextPage = pokeList.getLastPokemonOnListId();
-  pokeList.updatePokemons(nextPage);
+  const nextPage = pokemonList.getLastPokemonOnListId();
+  pokemonList.updatePokemons(nextPage);
 });
 $('#pokemon-list').on('click', (event) => {
   const pokemon = event.target.id;
   if (!event.target.classList.contains('btn-link')) return;
   getPokemonHandler(pokemon);
 });
-pokeList.updatePokemons();
+pokemonList.updatePokemons();
 
 // ## construir un pokedex
 // Documentacion https://pokeapi.co/
