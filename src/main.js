9d16820f487b6a16075fbb34b01ce17ea894dc66
Timothy <72170232+gruvix@users.noreply.github.com>
2023-11-15 16:04:30 -0300

    Revert "renamed loading functions to be more specific"
    This reverts commit 2e6d8c46c8ca11e30afc0e0bc75930f837115b12.

--- src/main.js
+++ src/main.js
@@ -70,15 +70,15 @@ function addPokemonToList(pokemon, index){
     const li = $('<li></li>')
     li.append(pokemonButton);
     $('#pokemon-list').append(li);
-    addLoadingToListItem(li)
+    addLoading(li)
     fetchSpriteToList(li, pokemon);
 }
-function addLoadingToListItem(element){
+function addLoading(element){
     const loading = $(`<div class="lds-dual-ring"></div>`);
     loading.addClass('lds-dual-ring');
     element.append(loading);
 }
-function removeLoadingFromListItem(element){
+function removeLoading(element){
     element.children('.lds-dual-ring').remove();
 }
 function fetchSpriteToList(element, pokemon){
@@ -86,7 +86,7 @@ function fetchSpriteToList(element, pokemon){
     .then(response => response.json())
     .then(data => {
         addSprite(data.sprites.front_default, element)
-        removeLoadingFromListItem(element)
+        removeLoading(element)
     })
     .catch(() => {
         alert('Could not get pokemon for sprite list');
