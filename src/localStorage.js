4c90958a07ef59cd29d7e9a927f1ee5a20647946
Gruvix <timmy.burton@hotmail.com>
2023-11-18 16:26:04 -0300

    add undefined paramenters error to load from storage functions

--- src/localStorage.js
+++ src/localStorage.js
@@ -17,12 +17,18 @@ function savePokemonListToLocalStorage(offset, amount, pokemons) {
   localStorage.setItem(getPokemonsKey(offset, amount), JSON.stringify(pokemons));
 }
 function loadPokemonFromLocalStorage(id) {
+  if (id === undefined) {
+    throw new Error('id is undefined');
+  }
   const pokemon = JSON.parse(localStorage.getItem(getPokemonKey(id)));
   if (pokemon === null) {
     throw new Error(`pokemon ${id} not found`);
   }
 }
 function loadPokemonListFromLocalStorage(offset, amount) {
+  if (offset === undefined || amount === undefined) {
+    throw new Error('offset or amount is undefined');
+  }
   const pokemons = JSON.parse(localStorage.getItem(getPokemonsKey(offset, amount)));
   if (pokemons === null) {
     throw new Error(`pokemon list with offset ${offset} and amount ${amount} not found`);
