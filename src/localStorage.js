cdfc0cde6878930654a44b47b709b150b97e4b91
Gruvix <timmy.burton@hotmail.com>
2023-11-18 16:00:57 -0300

    Create localStorage.js
    local storage contains all functions necessary functions to save and load pokemon locally

---
+++ src/localStorage.js
@@ -0,0 +1,30 @@
+function getPokemonKey(id) {
+  return `pokemon-${id}`;
+}
+function getPokemonsKey(offset, amount) {
+  return `pokemons-${offset}-to-${amount}`;
+}
+function savePokemonToLocalStorage(id, pokemon) {
+  if (id === undefined) {
+    throw new Error('id is undefined');
+  }
+  localStorage.setItem(getPokemonKey(id), JSON.stringify(pokemon));
+}
+function savePokemonListToLocalStorage(offset, amount, pokemons) {
+  if (offset === undefined || amount === undefined || pokemons !== Object) {
+    throw new Error('offset, amount or pokemons is undefined');
+  }
+  localStorage.setItem(getPokemonsKey(offset, amount), JSON.stringify(pokemons));
+}
+function loadPokemonFromLocalStorage(id) {
+  const pokemon = JSON.parse(localStorage.getItem(getPokemonKey(id)));
+  if (pokemon === null) {
+    throw new Error(`pokemon ${id} not found`);
+  }
+}
+function loadPokemonListFromLocalStorage(offset, amount) {
+  const pokemons = JSON.parse(localStorage.getItem(getPokemonsKey(offset, amount)));
+  if (pokemons === null) {
+    throw new Error(`pokemon list with offset ${offset} and amount ${amount} not found`);
+  }
+}
