cd6e486ea5b8fd799518135f3263f65d8fa2c45b
Gruvix <timmy.burton@hotmail.com>
2023-12-08 11:16:24 -0300

    remove all jest-related dependencies

--- src/__tests__/backpack.spec.js
+++
@@ -1,9 +0,0 @@
-import { hideBackback } from '../backpack.js';
-/// <reference types="Jest" />
-describe('it should test the backpack', () => {
-  it('it should hide the backpack', () => {
-    document.body.innerHTML ='<div id="pokemon-backpack-modal"></div>';
-    hideBackback();
-    expect($('#pokemon-backpack-modal').not.toBeVisible());
-  });
-});
